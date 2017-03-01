a1359b0e85c361d17356d3e0d5fa381533260921
ahodges <ahodges21@mail.student.gsu.edu>
2017-03-01 15:53:41 -0500

    Made byteToHv smaller

--- byteToHv.C
+++ byteToHv.C
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-void bytetohv(double byte, int channel)
+int bytetohv(double byte, int channel)
 {
   // check to see if byte is a natural number between 1 and 255
   if(byte != floor(byte) || byte < 1 || byte > 255)
     {
       cout << "Error, value must be an integer between 1 and 255." << endl;
-      exit(EXIT_FAILURE);
+      return 1;
     }
   
   //create array of coefficients that fit the equation coeff[n][0]*byte+coeff[n][1]
@@ -26,34 +26,18 @@ void bytetohv(double byte, int channel)
     {-0.0152,68.750},
   };
 
-  double volt;
-  
-  if(channel == 1)
-    {
 
-      volt = coeff[0][0]*byte + coeff[0][1];
+  if(channel < 5 && channel > 0)
+    {
+      double volt;
+      volt = coeff[channel-1][0]*byte + coeff[channel-1][1];
       cout << "The voltage attained by setting the byte to " << byte << " is " << volt << " volts +/- 0.0058%" << endl;
-    }
-  else if(channel == 2)
-    {
-      volt = coeff[1][0]*byte + coeff[1][1];
-      cout << "The voltage attained by setting the byte to " << byte << " is " << volt << " volts +/- 0.012%%" << endl;
-    }
-  else if(channel == 3)
-    {
-      volt = coeff[2][0]*byte + coeff[2][1];
-      cout << "The voltage attained by setting the byte to " << byte << " is " << volt << " volts +/- 0.0023%" << endl;
-    }
-  else if(channel == 4)
-    {
-      volt = coeff[3][0]*byte + coeff[3][1];
-      cout << "The voltage attained by setting the byte to " << byte << " " << "is " << volt <<" " <<  "volts +/- 0.001%" << endl;
     }
   else
     {
       cout << "Please input a channel value between 1 and 4." << endl;
     }
-
+  return 0;
   
 }
   
